d937007f889b3291ece2ebb83d2940a0249778ec
ulyanamnsrv <144213784+ulyanamnsrv@users.noreply.github.com>
2026-02-22 18:34:13 +0300

    Добавлены кнопки обновить и добавить локацию

---
+++ lab4/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.add-city').addEventListener('click', () => {
+    console.log('Добавить город');
+});
+
+document.querySelector('.refresh-all').addEventListener('click', () => {
+    console.log('Обновить всё');
+});
