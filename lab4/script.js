31d61189e4e184d48275f571758488da0cfeb58d
ulyanamnsrv <144213784+ulyanamnsrv@users.noreply.github.com>
2026-02-22 19:34:14 +0300

    Реализовано модальное окно с сохранением города

--- lab4/script.js
+++ lab4/script.js
@@ -1,7 +1,23 @@
+let modalOpen = false;
+
 document.querySelector('.add-city').addEventListener('click', () => {
-    console.log('Добавить город');
+    document.querySelector('.modal').classList.add('show');
+    modalOpen = true;
+});
+
+document.querySelector('.close').addEventListener('click', () => {
+    document.querySelector('.modal').classList.remove('show');
+    modalOpen = false;
+    document.querySelector('.city-input').value = '';
 });
 
 document.querySelector('.refresh-all').addEventListener('click', () => {
     console.log('Обновить всё');
+});
+
+document.querySelector('.save').addEventListener('click', () => {
+    const city = document.querySelector('.city-input').value.trim();
+    console.log('Город:', city);
+    document.querySelector('.modal').classList.remove('show');
+    document.querySelector('.city-input').value = '';
 });
