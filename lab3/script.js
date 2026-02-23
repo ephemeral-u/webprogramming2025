6edffb690854436e6a0852e0c649d12fe0c0613e
ulyanamnsrv <144213784+ulyanamnsrv@users.noreply.github.com>
2026-02-23 13:24:22 +0300

    Добавлена функция для добавления новых клеток после хода

--- lab3/script.js
+++ lab3/script.js
@@ -60,6 +60,8 @@ function moveUp() {
     }
     
     renderBoard();
+    addNewTile();
+    renderBoard();
 }
 
 function moveDown() {
@@ -86,6 +88,8 @@ function moveDown() {
         }
     }
     
+    renderBoard();
+    addNewTile();
     renderBoard();
 }
 
@@ -114,6 +118,8 @@ function moveRight() {
     }
     
     renderBoard();
+    addNewTile();
+    renderBoard();
 }
 
 function moveLeft() {
@@ -141,6 +147,23 @@ function moveLeft() {
     }
     
     renderBoard();
+    addNewTile();
+    renderBoard();
+}
+
+function addNewTile() {
+    let empty = [];
+    for(let i = 0; i < 4; i++) {
+        for(let j = 0; j < 4; j++) {
+            if(board[i][j] === 0) {
+                empty.push({i, j});
+            }
+        }
+    }
+    if(empty.length > 0) {
+        let pos = empty[Math.floor(Math.random() * empty.length)];
+        board[pos.i][pos.j] = Math.random() < 0.5 ? 2 : 4;
+    }
 }
 
 renderBoard();
